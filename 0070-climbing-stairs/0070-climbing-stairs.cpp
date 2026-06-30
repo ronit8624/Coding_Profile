7076b3a2a0e868dfa4341ded57b86497ed547483
ronit8624 <bhattacharyaronit86@gmail.com>
2026-06-30 12:27:02 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -15,7 +15,23 @@ public:
     }
 
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return f(n, dp);
+        // vector<int> dp(n+1, -1);
+        // dp[0] = 1;
+        // dp[1] = 1;
+
+        int prev1 = 1;
+        int prev2 = 1;
+
+        for(int i=2;i<=n;i++) {
+            // int step1 = dp[i-1];
+            // int step2 = dp[i-2];
+            // dp[i] = step1 + step2;
+
+            int cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1;
     }
 };
