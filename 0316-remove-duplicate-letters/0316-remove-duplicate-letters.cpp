0082b9d07fe505a3ec72e80613712d9cfc4009bb
ronit8624 <bhattacharyaronit86@gmail.com>
2026-07-20 01:53:44 +0530

    Time: 0 ms (100%), Space: 9.2 MB (68.82%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+
+        vector<int> lastIndex(26);
+        vector<bool> visited(26, false);
+
+        for(int i=0;i<n;i++) {
+            lastIndex[s[i] - 'a'] = i;
+        }
+
+        string ans = "";
+
+        for(int i=0;i<n;i++) {
+            char ch = s[i];
+
+            if(visited[ch - 'a']) continue;
+
+            while(!ans.empty() &&
+                  ans.back() > ch &&
+                  lastIndex[ans.back() - 'a'] > i) {
+
+                visited[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(ch);
+            visited[ch - 'a'] = true;
+        }
+
+        return ans;
+    }
+};
