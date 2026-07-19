67ac304a5148a75589e15d26f16e96900ca37391
ronit8624 <bhattacharyaronit86@gmail.com>
2026-07-19 14:21:28 +0530

    Time: 6 ms (11.62%), Space: 11.7 MB (52.14%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        stack<int> st;
+        st.push(-1);
+        int maxi = 0;
+
+        for(int i=0;i<n;i++) {
+            if(s[i] == '(') {
+                st.push(i);
+            }
+            else {
+                st.pop();
+
+                if(st.empty()) {
+                    st.push(i);
+                }
+                else {
+                    maxi = max(maxi, i - st.top());
+                }
+            }
+        }
+
+        return maxi;
+    }
+};
