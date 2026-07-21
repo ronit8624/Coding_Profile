e6bd01760b15ba9d6ef0f481548206b13950e095
ronit8624 <bhattacharyaronit86@gmail.com>
2026-07-21 19:07:36 +0530

    Time: 51 ms (24.5%), Space: 53.1 MB (10.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
-    int f(int ind, int target, vector<int>& coins, vector<vector<int>>& dp) {
-        if(ind == 0) {
-            if(target % coins[0] == 0) return target / coins[ind];
-            return 1e9;
-        }
-        if(dp[ind][target] != -1) return dp[ind][target];
-        int notTake = 0 + f(ind - 1, target, coins, dp);
-        int take = 1e9;
-        if(target >= coins[ind]) {
-            take = 1 + f(ind, target - coins[ind], coins, dp);
-        }
-        return dp[ind][target] = min(notTake, take);
-    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(amount + 1, 0));
+
         for(int target = 0; target <= amount; target++) {
-            if(target % coins[0] == 0) dp[0][target] = target / coins[0];
-            else dp[0][target] = 1e9;
-        }
-        for(int ind = 1; ind < n; ind ++) {
-            for(int target = 0; target <= amount; target++) {
-                int notTake = 0 + dp[ind - 1][target];
-                int take = 1e9;
-                if(target >= coins[ind]) {
-                    take = 1 + dp[ind][target - coins[ind]];
-                }
-                dp[ind][target] = min(notTake, take);
+            if(target % coins[0] == 0) {
+                dp[0][target] = target / coins[0];
+            }
+            else {
+                dp[0][target] = 1e9;
             }
         }
-        return dp[n-1][amount] >= 1e9 ? -1 : dp[n-1][amount];
+
+        for(int i=1;i<n;i++) {
+            for(int target = 0;target<=amount;target++) {
+                int notTake = dp[i-1][target];
+                int take = 1e9;
+
+                if(target >= coins[i]) {
+                    take = 1 + dp[i][target - coins[i]];
+                }
+
+                dp[i][target] = min(take, notTake);
+            }
+        }
+
+        if(dp[n-1][amount] == 1e9) return -1;
+        return dp[n-1][amount];
     }
 };
