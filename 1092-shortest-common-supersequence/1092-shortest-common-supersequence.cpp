1d32831d786a20d51b36b1c11eda116d546b939a
ronit8624 <bhattacharyaronit86@gmail.com>
2026-07-21 22:02:29 +0530

    Time: 27 ms (36.58%), Space: 26.7 MB (90.51%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int m = str1.size();
-        int n = str2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        int n = str1.size();
+        int m = str2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
 
-        for(int j=0;j<=n;j++) dp[0][j] = 0;
-        for(int i=0;i<=m;i++) dp[i][0] = 0;
-
-        for(int i=1;i<=m;i++) {
-            for(int j=1;j<=n;j++) {
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
                 if(str1[i-1] == str2[j-1]) {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
@@ -20,12 +17,13 @@ public:
         }
 
         string ans = "";
-        int i = m, j = n;
+        int i=n, j=m;
 
         while(i > 0 && j > 0) {
             if(str1[i-1] == str2[j-1]) {
                 ans += str1[i-1];
-                i--;j--;
+                i--;
+                j--;
             }
             else if(dp[i-1][j] > dp[i][j-1]) {
                 ans += str1[i-1];
