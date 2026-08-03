f87352c583b1ab2a4f601aba0befbcccebe32a52
ronit8624 <bhattacharyaronit86@gmail.com>
2026-08-03 11:21:20 +0530

    Time: 64 ms (83.85%), Space: 82 MB (89.14%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -2,31 +2,27 @@ class Solution {
 public:
     int minSetSize(vector<int>& arr) {
         int n = arr.size();
-        unordered_map<int, int> mpp;
-
-        for(int i=0;i<n;i++) {
-            mpp[arr[i]]++;
-        }
-
-        vector<int> cntel;
-
-        for(auto& it : mpp) {
-            cntel.push_back(it.second);
-        }
-
-        sort(cntel.begin(), cntel.end(), greater<int>());
-
         int target = n / 2;
-        int removed = 0;
-        int ans = 0;
+        int removed = 0, ans = 0;
 
-        for(int i=0;i<cntel.size();i++) {
-            removed += cntel[i];
+        unordered_map<int, int> freqMap;
+        for(int i=0;i<n;i++) {
+            freqMap[arr[i]]++;
+        }
+
+        vector<int> freq;
+        for(auto it : freqMap) {
+            freq.push_back(it.second);
+        }
+
+        sort(freq.begin(), freq.end(), greater<int>());
+
+        for(int i=0;i<freq.size();i++) {
+            removed += freq[i];
             ans++;
 
-            if(removed >= target) {
+            if(removed >= target)
                 break;
-            }
         }
 
         return ans;
