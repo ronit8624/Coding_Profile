1c8668f5dc67f1a225e32f02eb6f578323ac7a2e
ronit8624 <bhattacharyaronit86@gmail.com>
2026-08-04 19:57:09 +0530

    Time: 13 ms (42.75%), Space: 17.2 MB (10.76%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n1 = text1.size();
-        int n2 = text2.size();
+    int lcs(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
-
-        // return f(n1, n2, text1, text2, dp);
-        for(int ind2 = 0; ind2 <= n2; ind2++) dp[0][ind2] = 0;
-        for(int ind1 = 0; ind1 <= n1; ind1++) dp[ind1][0] = 0;
-        
-        for(int ind1 = 1; ind1 <= n1; ind1++) {
-            for(int ind2 = 1; ind2 <= n2; ind2++) {
-                int notMatch1 = dp[ind1-1][ind2];
-                int notMatch2 = dp[ind1][ind2-1];
-
-                int match = 0;
-                if(text1[ind1 - 1] == text2[ind2 - 1]) {
-                    match = 1 + dp[ind1-1][ind2-1];
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
-
-                dp[ind1][ind2] = max(match, max(notMatch1, notMatch2));
+                else {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
 
-        return dp[n1][n2];
+        return dp[n][m];
     }
 
     int minDistance(string word1, string word2) {
-        int lcs = longestCommonSubsequence(word1, word2);
-        return (word1.size() - lcs) + (word2.size() - lcs);
+        int lcss = lcs(word1, word2);
+        return (word1.size() - lcss) + (word2.size() - lcss);
     }
 };
