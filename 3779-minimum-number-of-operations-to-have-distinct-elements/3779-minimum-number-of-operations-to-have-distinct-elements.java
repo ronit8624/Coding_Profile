7bd9761e81e5c2d1121c1382b48d7017d7f0299f
ronit8624 <bhattacharyaronit86@gmail.com>
2026-09-05 19:41:39 +0530

    Time: 10 ms (78.4%), Space: 139.2 MB (71.6%) - LeetHub

---
+++ 3779-minimum-number-of-operations-to-have-distinct-elements/3779-minimum-number-of-operations-to-have-distinct-elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = nums.length - 1; i >= 0; i--) {
+            if(set.contains(nums[i])) {
+                return i / 3 + 1;
+            }
+
+            set.add(nums[i]);
+        }
+
+        return 0;
+    }
+}
