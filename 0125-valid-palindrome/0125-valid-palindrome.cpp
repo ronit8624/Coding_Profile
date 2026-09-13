2b6948e27436c01be160ece2df5fbbb2e8b0af5a
ronit8624 <bhattacharyaronit86@gmail.com>
2026-09-13 22:36:06 +0530

    Time: 2 ms (35.08%), Space: 10.2 MB (28.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0, right = s.length() - 1;
+        string str = "";
 
-        while(left < right) {
-            char l = s[left];
-            char r = s[right];
-
-            if(!isAlphaNum(l)) {
-                left++;
-            }
-            else if(!isAlphaNum(r)) {
-                right--;
-            }
-            else {
-                if(tolower(l) != tolower(r)) {
-                    return false;
-                }
-
-                left++;
-                right--;
+        for(int i=0;i<s.size();i++) {
+            if(isalnum(s[i])) {
+                str.push_back(tolower(s[i]));
             }
         }
 
-        return true;
-    }
+        int i = 0, j = str.size() - 1;
+        while(i < j) {
+            if(str[i] != str[j]) return false;
+            i++;
+            j--;
+        }
 
-    bool isAlphaNum(char c) {
-        return (c >= 'a' && c <= 'z') ||
-               (c >= 'A' && c <= 'Z') ||
-               (c >= '0' && c <= '9');
+        return true;
     }
 };
