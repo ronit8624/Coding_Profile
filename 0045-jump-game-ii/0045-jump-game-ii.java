fb3976af6cc3728774ef67629b85cb748d0a37b7
ronit8624 <bhattacharyaronit86@gmail.com>
2026-09-18 23:44:28 +0530

    Time: 1 ms (99.8%), Space: 47.3 MB (57.68%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int jumps = 0;
-        int maxReach = 0;
-        int curEnd = 0;
+        int jumps = 0, maxReach = 0, currEnd = 0;
 
-        for(int i = 0; i < n - 1; i++) {
+        for(int i=0;i<n-1;i++) {
             maxReach = Math.max(maxReach, i + nums[i]);
 
-            if(i == curEnd) {
+            if(i == currEnd) {
                 jumps++;
-                curEnd = maxReach;
+                currEnd = maxReach;
             }
         }
 
