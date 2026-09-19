6d7dcff729323a012233c13e9e4c19dace3216e5
ronit8624 <bhattacharyaronit86@gmail.com>
2026-09-19 15:39:55 +0530

    Time: 23 ms (28.75%), Space: 47.8 MB (9.59%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> st = new Stack<>();
+
+        for(int i=0;i<s.length();i++) {
+            if(sb.charAt(i) == '(') {
+                st.push(i);
+            }
+
+            if(sb.charAt(i) == ')') {
+                if(!st.isEmpty()) {
+                    st.pop();
+                }
+                else {
+                    sb.setCharAt(i, '*');
+                }
+            }
+        }
+
+        while(!st.isEmpty()) {
+            sb.setCharAt(st.pop(), '*');
+        }
+
+        return sb.toString().replaceAll("\\*","");
+    }
+}
